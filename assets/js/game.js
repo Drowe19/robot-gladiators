ce723cefe81b302235f950b2fbd4fa6559f1761e
Drowe19 <Danrowe520@gmail.com>
2021-11-27 10:18:32 -0500

    Added shop functions to upgrade robots attributes

--- assets/js/game.js
+++ assets/js/game.js
@@ -164,11 +164,22 @@ for (var i =0; i <enemyNames.length; i++){
     
     //pass the pickedEnemyName variable's value into the fight function call where it will assume the value of the enemy name parameter
     fight(pickedEnemyName);
+    //if we're not at the last enemy in the array
+        if (playerHealth > 0 && i < enemyNames.length - 1){
+            //ask if players wants to use the store before next round
+            var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+            //if yes,(true) take them to the store() function
+            if (storeConfirm){
+                shop();
+            }
+        }
     }
-    else {
+        else {
         window.alert("You have lost your robot in battle! Game Over!");
         break;
-    }
+        }
+    
 }
 //After the loop ends, the player is either out of health, or enemies to fight, so run the endgame function
 endGame();
@@ -179,7 +190,7 @@ var endGame = function(){
     window.alert("The game has now ended. Let's see how you did!");
     //if player is still alive, player wins!
     if (playerHealth > 0){
-        window.alert("Great job, you've survived the game! You now have a score of" + playerMoney + ".");
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
     }
     else{
         window.alert("You've lost your robot in battle.");
@@ -197,5 +208,55 @@ var endGame = function(){
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 };
+
+var shop = function(){
+    //ask player what they'd like to do
+    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+
+
+    //1)    use switch to carry out action. Whatever value user enters for shopOptionPrompt will execute said case scenario
+    //2)    Nest if statements to stop player from upgrading robot if there is not enough money available
+    //3)    Account for player entering captial or lowercase input. Enter second case scenario
+    switch (shopOptionPrompt){
+        case "REFILL":  //new case
+        case "refill":
+            if (playerMoney >= 7){
+            window.alert("Refulling a player's health by 20 for 7 dollars.");
+
+            //increase health and decrease money 
+            playerHealth = playerHealth + 20;
+            playerMoney = playerMoney - 7;
+            }
+            else{
+                window.alert("You don't have enough money!");
+            }
+            break;
+        case "UPGRADE": //new case
+        case "upgrade":
+            
+            if (playerMoney >=7){
+            window.alert("Upgrade player's attack by 6 for 7 dollars.");
+            //increase attack and decrease money 
+            playerAttack = playerAttack + 6;
+            playerAttack = playerMoney - 7;
+            }
+            else{
+                window.alert("You don't have enough money!");
+            }
+            break;
+        case "LEAVE":   //new case
+        case "leave":
+            window.alert("Leaving the store.");
+            
+            //do nothing, so function will end
+            break;
+        default:
+            window.alert("You did not pick a valid option. Try again.");
+
+            //call shop() again to force player to pick a valid option
+            shop();
+            break;
+    }
+}; 
 //start the game when the page loads
 startGame();
