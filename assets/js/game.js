267f95e4d0186a16514d010fd3235a79889638c3
Drowe19 <Danrowe520@gmail.com>
2021-12-14 18:13:39 -0500

    Save and Load High Score

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,8 +71,6 @@ var fight = function(enemy) {
                 //if true, leave fight by breaking loop
                 break;
             }
-        }
-
     
     //generate random damage value based on player's attack power
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
@@ -118,8 +116,9 @@ var fight = function(enemy) {
         }
     }
     //switch turn order for next round
-    isplayerTurn = !isPlayerTurn;
-    };
+    isPlayerTurn = !isPlayerTurn;
+    }
+};
     
     
 
@@ -177,27 +176,37 @@ endGame();
 //function to end the entire game
 var endGame = function(){
     window.alert("The game has now ended. Let's see how you did!");
-    //if player is still alive, player wins!
-    if (playerInfo.health > 0){
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
-    }
-    else{
-        window.alert("You've lost your robot in battle.");
-    }
-//Might want to add the end function brace after the else statement below
 
-    //ask player if they'd like to play again might want to comment this all out if its not in the right place. Does this go after the startgame function expression? or nested within it
+
+    //checklocalStorage for a high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null){
+        highScore = 0;
+    }
+
+    //if player has more money than the high score, player has new high score!
+    if (playerInfo.money > highScore){
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name); 
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
+    else { 
+        alert(playerInfo.name + "did not beat the high score of " + highScore + ".Maybe next time!");
+    }
+
+    // ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
     if (playAgainConfirm){
-        //restart the game
         startGame();
     }
-    else{
+    else {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 };
 
+    
 var shop = function(){
     //ask player what they'd like to do
     var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 1 for REFILL, 2 for UPGRADE , or 3 to LEAVE");
@@ -310,6 +319,8 @@ var enemyInfo = [
 
 //start the game when the page loads
 startGame();
+
+
 
 
 
